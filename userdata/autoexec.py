dd39ec4398f1ce25b33cf4ff2fc66ef6f4133a76
lulo_client <gtrujillos@gmail.com>
2019-09-22 10:57:53 -0500

    first commit

---
+++ userdata/autoexec.py
@@ -0,0 +1,4 @@
+import xbmc
+#xbmc.executebuiltin( "PlayMedia(/home/osmc/.kodi/userdata/playlists/video/miplaylist.m3u)" )
+xbmc.executebuiltin( "PlayMedia(/home/osmc/luloclient/media/files/lulo_playlist.m3u)" )
+xbmc.executebuiltin( "PlayerControl(repeatall)" )
